d034b6502f78ed4e966a3522ac977600e8a3f046
Connor Walsh <cwalsh24@ut.utm.edu>
2022-09-16 10:05:37 -0500

    stacker with constructor

---
+++ stacker.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file stacker.cpp
+ * @author Connor Walsh
+ * @date 2022-09-14
+ * @brief This is the stacker source code
+ * 
+ * stuff
+ */
+
+#include "stacker.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string> 
+
+using namespace std;
+
+Stacker(){
+  magic_number; 
+  width = 0;
+  height = 0;
+  max_color = 255;
+}
+
