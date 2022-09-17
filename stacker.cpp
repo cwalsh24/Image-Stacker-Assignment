a3e7948659996799fe8cc691ec680ae977a4b2b9
Connor Walsh <cwalsh24@ut.utm.edu>
2022-09-17 13:13:40 -0500

    Added a rough idea of how to read in the correct file names

--- stacker.cpp
+++ stacker.cpp
@@ -20,19 +20,33 @@ Stacker::Stacker(){
   width = 0;
   height = 0;//height before width
   max_color = 255; 
-  //pixels; 
+  pixels.red = 0;
+  pixels.green = 0;
+  pixels.blue = 0; 
 }
 
 void Stacker::fileLoader(){
   int temporary = 0;
   ifstream in;
   string fileName;
-  string folderName; 
+  string folderName;
+  int numberOfImages; 
   cout << "Please enter the image you wish to stack: "; 
-  cin >> folderName; 
-
+  cin >> folderName;
+  //getline();? Might need this to get rid of the space
   
-  fileName = folderName + "/" + folderName + "_0" + endNumber + ".ppm"; //this will get the file name, but we need to figure out how to get the numbers 
+  cout << "Please enter the number of images: ";
+  cout >> numberOfImages;
+  //Something like these will get the correct file name depending on the number of files the user wants to get.
+  //We might want to put in an else statement to end the method if the number is less than one or more than 99 
+  if(numberOfImages < 10){
+    fileName = folderName + "/" + folderName + "_00" + numberOfImages + ".ppm";
+  }
+  
+  if(10 <= numberOfImages){
+    fileName = folderName + "/" + folderName + "_00" + numberOfImages + ".ppm";
+  }
+
   in.open(fileName.c_str());
 
   int count = 0;
