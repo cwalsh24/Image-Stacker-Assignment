41d58ba5278829d9cdfbf5afa63916afc1824a0e
Connor Walsh <cwalsh24@ut.utm.edu>
2022-09-19 13:11:49 -0500

    main

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
  * @file main.cpp
  * @author Code4Days (Isaac Copeland, Connor Walsh)
  * @date 2022-09-19
-<<<<<<< HEAD
  * @brief driver code for the Stacker class
  * 
  * This program receives input for .ppm files to be opened and averaged to clean up an image.
@@ -10,32 +9,17 @@
 
 
 #include "Vector.h"
-=======
- * @brief driver code for stacker class
- * 
- * This program receives input for .ppm files to be opened and averagd to clean up an image.
- */
-
-
-#include <iostream>
->>>>>>> b1ddd69965e2570943bd444001e573becd8821a2
 
 using namespace std;
 
 int main() {
-<<<<<<< HEAD
-  Stack s; 
+  Stack s;
   string folderName;
   int numberOfImages;
   cout << "Please enter the image you wish to stack: ";
   cin >> folderName;
   cout << "Please enter the number of images: ";
   cout >> numberOfImages;
-  s.fileLoader(folderName, numberOfImages);  
+  s.fileLoader(folderName, numberOfImages);
 
-=======
-  
-  
-  return 0;
->>>>>>> b1ddd69965e2570943bd444001e573becd8821a2
 }
