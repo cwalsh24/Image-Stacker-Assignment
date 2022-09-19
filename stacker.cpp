1916213a1edf11e2256f0a68a325f9ccf9383b31
Connor Walsh <cwalsh24@ut.utm.edu>
2022-09-19 13:23:59 -0500

    This has correct while loop logic for the file read, the user input has been moved to the main

--- stacker.cpp
+++ stacker.cpp
@@ -7,11 +7,7 @@
  * stuff
  */
 
-#include "stacker.h" 
-#include <iostream> //if these are in the .h we don't need them 
-#include <fstream>
-#include <vector>
-#include <string> 
+#include "stacker.h"  
 
 using namespace std;
 
@@ -20,29 +16,17 @@ Stacker::Stacker(){
   width = 0;
   height = 0;//height before width
   max_color = 255; 
-  //pixels.red = 0;
-  //pixels.green = 0;
-  //pixels.blue = 0;
-  //pixel; do any of these need to be initialized here?  
+  pixel p;   
 }
 
-void Stacker::fileLoader(){ //parameters (vector<pixel> & vec, string folderName, int numberOfImages) 
-  int temporary = 0;
+void Stacker::fileLoader(string folderName, int numberOfImages, pixel p){  
+  //int temporary = 0; may not be needed
   ifstream in;
-  string fileName; 
-  string folderName;//this and the numberOfImages variable might need to go in the main
-  //something like this will be used for reading in all the files
-  int numberOfImages; 
-  cout << "Please enter the image you wish to stack: "; 
-  cin >> folderName;
-  //getline();? Might need this to get rid of the space
-  
-  cout << "Please enter the number of images: ";
-  cout >> numberOfImages;
+  string fileName;
   
   //Something like these will get the correct file name depending on the number of files the user wants to get.
   //We might want to put in an else statement to end the method if the number is less than one or more than 99 
-  if(numberOfImages < 10){
+  if(numberOfImages < 10){ 
     fileName = folderName + "/" + folderName + "_00" + numberOfImages + ".ppm";
     cout << fileName; 
   }
@@ -51,27 +35,21 @@ void Stacker::fileLoader(){ //parameters (vector<pixel> & vec, string folderName
     fileName = folderName + "/" + folderName + "_00" + numberOfImages + ".ppm";
     cout << fileName; 
   } //when these run we might want to do a numberOfimages-- and then call the method again at the end
+  //or we could do like numberOfImages-(numberOfimages - 1) and count up. The example went from 1 to 10  
   
   in.open(fileName.c_str());
+ 
+  //this line reads in the values for these variables from the file. 
+  in >> magic_number >> height >> width >> max_color;  //**note the magic_number might want to be a priming read
 
-  int count = 0; //I thought of using this to prevent the loop from trying to read in height/P3/width again
-  //in >> temporary; //this is a priming read
-  while(in){
-    getline(in, magic_number); //something like this should get the P3 value;
-    
-    //in >> height;  //in.ignore() might be worth looking into for dealing with the spaces
-    //in >> width;
+  while(in){ //priming read? 
+    //this puts values into the vector of structs.  
+    in >> p2.red;
+    in >> p2.green;
+    in >> p2.blue;
+    vec2.push_back(p2);
 
-    while(in){
-      //this should put values into the vector of structs; 
-      in >> vec.r[count];
-      in >> vec.g[count];
-      in >> vec.b[count];
-      count++; 
-
-    }
-    
-  }
+  }  
 }
 
 void Stacker::Average(vector<pixel> &vec){ //we are probably going to end up with a vector for each file read 
